00f03f22aaaca9ad19a9f8fd9526dd2ad9f43207
duahauthom2009-beep <duahauthom2009@gmail.com>
2026-02-23 18:21:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-function showTab(tab) {
-
-    document.querySelectorAll(".tab").forEach(t=>{
-        t.classList.add("hidden");
-    });
-
-    document.getElementById(tab).classList.remove("hidden");
+function toggleTheme(){
+    document.body.classList.toggle("tet");
+}
+body.tet {
+    background: linear-gradient(135deg,#ff512f,#dd2476);
 }
